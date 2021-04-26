4584b2a64bc7f407b3e20e8d4ce3336d38099985
Jingzhao Zhang <61613079+4065-zjz@users.noreply.github.com>
2021-04-26 16:25:59 +0800

    Delete Pricing_European_Options directory

--- Pricing_European_Options/BinModel01.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-//computing risk-neutral probability
-double RiskNeutProb(double U, double D, double R);
-
-//computing the stock price at node n,i
-double S(double S0, double U, double D, int n, int i);
-
-//inputting, displaying and checking model data
-int GetInputData(double& S0, double& U, double& D, double& R);
