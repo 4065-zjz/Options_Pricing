54f5f3bb394e9cfb4393f69fb4e4f16e3186b368
Jingzhao Zhang <61613079+4065-zjz@users.noreply.github.com>
2021-04-26 16:23:47 +0800

    Add files via upload
    Pricing European Call/Put options

---
+++ Pricing_European_Options/BinModel01.h
@@ -0,0 +1,9 @@
+#pragma once
+//computing risk-neutral probability
+double RiskNeutProb(double U, double D, double R);
+
+//computing the stock price at node n,i
+double S(double S0, double U, double D, int n, int i);
+
+//inputting, displaying and checking model data
+int GetInputData(double& S0, double& U, double& D, double& R);
